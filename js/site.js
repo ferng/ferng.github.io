9f2d640113262538b46b53a13336fc175490bc44
ferng001 <ferng001@gmail.com>
2017-09-08 15:23:09 +0100

    update index

--- js/site.js
+++ js/site.js
@@ -1,10 +1,30 @@
-
 window.addEventListener('scroll', function() {
     var navElement = document.querySelector('.navbar');
-    var docTop = document.querySelector('body').scrollTop;
-    if (docTop > 660) {
+    var introElement = document.getElementById('introSection');
+    var navPos = navElement.getBoundingClientRect();
+    var introPos = introElement.getBoundingClientRect();
+    if (navPos.top < 1) {
         navElement.classList.add('fixed-header');
-    } else {
+    } 
+    if (navPos.top == 0 && introPos.top > 0) {
         navElement.classList.remove('fixed-header');
     }
 });
+
+window.onload = function() {
+    var links = document.querySelectorAll('.navbar-link');
+    links.forEach(function(link) {
+        link.addEventListener('click', displaySection);
+    })
+
+}
+
+function displaySection() {
+    var sections = document.querySelectorAll('.docs-section');
+    sections.forEach(function(section) {
+        section.style.display = 'none';
+    })
+
+    var displaySection = document.getElementById(this.id + 'Section');
+    displaySection.style.display = 'block';
+}
