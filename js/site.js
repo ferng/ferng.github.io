a632584d8a5945842bc1bdfac9a60835695de6c8
ferng001 <ferng001@gmail.com>
2017-09-12 16:31:44 +0100

    polyfill as IE/edge don't support foreach

--- js/site.js
+++ js/site.js
@@ -12,6 +12,15 @@ window.addEventListener('scroll', function() {
 });
 
 
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
 window.onload = function() {
     var links = document.querySelectorAll('.internal');
     links.forEach(function(link) {
